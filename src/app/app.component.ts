aa240bef86103f412738dac5d677684540dd3c87
Benighter <111303968+Benighter@users.noreply.github.com.>
2024-11-20 06:40:40 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './components/navigation/navigation.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavigationComponent],
+  template: `
+    <app-navigation></app-navigation>
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+  `,
+  styles: [`
+    main {
+      min-height: calc(100vh - 64px);
+      background-color: #f8f9fa;
+    }
+  `]
+})
+export class AppComponent {}
